3f5fe46c20df056fcdcfa71e75433dfba8220ab9
Billybobchops <chlanda52@gmail.com>
2025-01-31 16:54:54 -0500

    fix typo

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -5,7 +5,7 @@ export default createModuleFederationConfig({
     remotes: {
         remoteDesignSystem:
             process.env.NODE_ENV === 'production'
-                ? 'remoteDesignSystem@https://starling-ui-design-system.netlify.app/rmf-manifest.json'
+                ? 'remoteDesignSystem@https://starling-ui-design-system.netlify.app/mf-manifest.json'
                 : 'remoteDesignSystem@http://localhost:3001/mf-manifest.json',
     },
     shared: {
