f62dd66e5ff6d331f2c7bbcba081d8a641fa749c
Billybobchops <chlanda52@gmail.com>
2025-02-19 09:44:41 -0500

    move dummy data out of file

---
+++ src/dummyData.ts
@@ -0,0 +1,55 @@
+export const dummyData = {
+    navLinks: [
+        {
+            label: 'My Account',
+            href: '/account',
+            subMenu: [
+                { label: 'View or Pay Open Invoices', href: '/account/openinvoices' },
+                { label: 'View or Pay Closed Invoices', href: '/account/closedinvoices' },
+                { label: 'View Scheduled Payments', href: '/account/scheduledpayments' },
+                { label: 'Payment History', href: '/account/paymenthistory' },
+            ],
+        },
+        {
+            label: 'My Profile',
+            href: '/profile',
+            subMenu: [
+                { label: 'AutoPay', href: '/profile/autopay' },
+                { label: 'Change Password', href: '/profile/changepassword' },
+                { label: 'Manage Accounts', href: '/profile/manageaccounts' },
+                { label: 'Manage Users', href: '/profile/manageusers' },
+                { label: 'Paperless', href: '/profile/paperless' },
+                { label: 'Payment Methods', href: '/profile/paymentmethods' },
+                { label: 'Pay by Text', href: '/profile/paybytext' },
+                { label: 'Recurring Scheduled Payments', href: '/profile/recurringscheduledpayments' },
+                { label: 'Update Account Info', href: '/profile/updateaccountinfo' },
+                { label: 'Update My User Info', href: '/profile/updateuserinfo' },
+            ],
+        },
+        {
+            label: 'Support',
+            href: '/support',
+            subMenu: [
+                {
+                    label: 'Help - User Videos',
+                    href: '/support/uservideos',
+                    subMenu: [
+                        { label: 'Account Linking', href: '/support/uservideos/accountlinking' },
+                        { label: 'Managing Your InvoiceCloud Account', href: '/support/uservideos/accountmanagement' },
+                        {
+                            label: 'How to Update Your AutoPay Payment Method',
+                            href: '/support/uservideos/autopayupdate',
+                        },
+                        { label: 'How to Register for Pay by Text', href: '/support/uservideos/registerpaybytext' },
+                        { label: 'Making a Payment using Pay by Text', href: '/support/uservideos/paymentpaybytext' },
+                        { label: 'Make Payments using PayPal', href: '/support/uservideos/paymentpaypal' },
+                        { label: 'Make Payments using Venmo', href: '/support/uservideos/paymentvenmo' },
+                        { label: 'Make Payments using Google Pay', href: '/support/uservideos/paymentgooglepay' },
+                        { label: 'Make Payments using Apple Pay', href: '/support/uservideos/paymentapplepay' },
+                    ],
+                },
+            ],
+        },
+        { label: 'Sign Out', href: '/signout' },
+    ],
+};
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,5 @@
 import logo from '@/assets/example-logo.png';
+import { dummyData } from '@/dummyData';
 import Footer from '@components/Footer';
 import Sidebar from '@components/Sidebar';
 import { Outlet } from '@modern-js/runtime/router';
@@ -9,60 +10,6 @@ import Paragraph from 'remoteDesignSystem/Paragraph';
 import SecondaryButton from 'remoteDesignSystem/SecondaryButton';
 
 export default function Layout() {
-    const navLinks = [
-        {
-            label: 'My Account',
-            href: '/account',
-            subMenu: [
-                { label: 'View or Pay Open Invoices', href: '/account/openinvoices' },
-                { label: 'View or Pay Closed Invoices', href: '/account/closedinvoices' },
-                { label: 'View Scheduled Payments', href: '/account/scheduledpayments' },
-                { label: 'Payment History', href: '/account/paymenthistory' },
-            ],
-        },
-        {
-            label: 'My Profile',
-            href: '/profile',
-            subMenu: [
-                { label: 'AutoPay', href: '/profile/autopay' },
-                { label: 'Change Password', href: '/profile/changepassword' },
-                { label: 'Manage Accounts', href: '/profile/manageaccounts' },
-                { label: 'Manage Users', href: '/profile/manageusers' },
-                { label: 'Paperless', href: '/profile/paperless' },
-                { label: 'Payment Methods', href: '/profile/paymentmethods' },
-                { label: 'Pay by Text', href: '/profile/paybytext' },
-                { label: 'Recurring Scheduled Payments', href: '/profile/recurringscheduledpayments' },
-                { label: 'Update Account Info', href: '/profile/updateaccountinfo' },
-                { label: 'Update My User Info', href: '/profile/updateuserinfo' },
-            ],
-        },
-        {
-            label: 'Support',
-            href: '/support',
-            subMenu: [
-                {
-                    label: 'Help - User Videos',
-                    href: '/support/uservideos',
-                    subMenu: [
-                        { label: 'Account Linking', href: '/support/uservideos/accountlinking' },
-                        { label: 'Managing Your InvoiceCloud Account', href: '/support/uservideos/accountmanagement' },
-                        {
-                            label: 'How to Update Your AutoPay Payment Method',
-                            href: '/support/uservideos/autopayupdate',
-                        },
-                        { label: 'How to Register for Pay by Text', href: '/support/uservideos/registerpaybytext' },
-                        { label: 'Making a Payment using Pay by Text', href: '/support/uservideos/paymentpaybytext' },
-                        { label: 'Make Payments using PayPal', href: '/support/uservideos/paymentpaypal' },
-                        { label: 'Make Payments using Venmo', href: '/support/uservideos/paymentvenmo' },
-                        { label: 'Make Payments using Google Pay', href: '/support/uservideos/paymentgooglepay' },
-                        { label: 'Make Payments using Apple Pay', href: '/support/uservideos/paymentapplepay' },
-                    ],
-                },
-            ],
-        },
-        { label: 'Sign Out', href: '/signout' },
-    ];
-
     // To Do: I believe this is custom CRM content... we may need to build a utility function to convert it to our components
     // We may want to look at generating the content in the component file somehow while still conditionally rendering it here
     const sidebarContent = (
@@ -83,7 +30,7 @@ export default function Layout() {
 
     return (
         <>
-            <Header homeLink={'/home'} logoAltText="Invoice Cloud logo" logoSrc={logo} navLinks={navLinks} />
+            <Header homeLink={'/home'} logoAltText="Invoice Cloud logo" logoSrc={logo} navLinks={dummyData.navLinks} />
             <div className="layout">
                 <div className="content">
                     <Outlet />
